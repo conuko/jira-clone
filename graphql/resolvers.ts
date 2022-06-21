12e662bf9bc89fea602a0a8857ce19b2c0d7e4e0
conuko <constantinu93@gmail.com>
2022-06-21 18:13:33 +0200

    feat: set up graphql, nexus, apollo-server

---
+++ graphql/resolvers.ts
@@ -0,0 +1,11 @@
+import prisma from "../lib/prisma";
+
+export const resolvers = {
+  Query: {
+    tasks: async (
+      _parent: any,
+      args: any,
+      ctx: { prisma: { task: { findMany: () => any } } }
+    ) => await ctx.prisma.task.findMany(),
+  },
+};
