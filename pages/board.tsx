368d288b0bcba782af6c319fb0e063c530ff3d40
conuko <constantinu93@gmail.com>
2022-06-23 09:00:03 +0200

    feat: create Task Component and reusable type

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { gql, useQuery } from "@apollo/client";
+import { Row } from "react-bootstrap";
 
 const AllTasksQuery = gql`
   query {
@@ -18,7 +19,20 @@ const Board = () => {
       console.log(data);
     },
   });
-  return <div>Board</div>;
+  return (
+    <div className="pt-3 h-100 d-flex flex-column">
+      <Row>
+        <h1>Project Title</h1>
+      </Row>
+      <div className="board-container">
+        {data &&
+          data.tasks.map((task) => {
+            return <div key={task.title}>{task.title}</div>;
+          })}
+      </div>
+      Board
+    </div>
+  );
 };
 
 export default Board;
