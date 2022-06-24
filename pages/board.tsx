b10a488a7b897b1c8581b6829d33f43a1e62c370
conuko <constantinu93@gmail.com>
2022-06-24 18:21:09 +0200

    feat: finish the board page

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Row } from "react-bootstrap";
+import TaskComponent from "../components/TaskComponent";
+import BoardSection from "../components/BoardSection";
 
 const AllTasksQuery = gql`
   query {
@@ -19,18 +21,29 @@ const Board = () => {
       console.log(data);
     },
   });
+
+  const sections: Array<String> = ["Backlog", "In-Progress", "Review", "Done"];
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
   return (
     <div className="pt-3 h-100 d-flex flex-column">
       <Row>
         <h1>Project Title</h1>
       </Row>
-      <div className="board-container">
-        {data &&
-          data.tasks.map((task) => {
-            return <div key={task.title}>{task.title}</div>;
-          })}
+      <div className="board-container d-flex flex-row flex-grow-1">
+        {sections.map((section: String, index: number) => {
+          let filteredData: Array<Task> = data
+            ? data.tasks.filter((task: Task) => {
+                return task.status === section;
+              })
+            : [];
+          return (
+            <BoardSection title="Backlog" tasks={filteredData} key={index} />
+          );
+        })}
       </div>
-      Board
     </div>
   );
 };
