5141bee7fc19fbbd78b23094219e81bf1b1aa25d
conuko <constantinu93@gmail.com>
2022-07-12 18:24:44 +0200

    feat: change prisma schema to match next-auth model

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ApolloProvider } from "@apollo/client";
-import apolloClient from "../lib/apollo";
 import Layout from "../components/layout";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { ApolloProvider } from "@apollo/client";
+import apolloClient from "../lib/apollo";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <ApolloProvider client={apolloClient}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Container className="main-content">
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Container>
       </ApolloProvider>
     </SessionProvider>
   );
