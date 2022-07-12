a74c39a49810d0e5b3136c658168c8d65b997ff9
conuko <constantinu93@gmail.com>
2022-07-12 16:50:01 +0200

    feat: finish email setup for next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,17 @@ import { ApolloProvider } from "@apollo/client";
 import apolloClient from "../lib/apollo";
 import Layout from "../components/layout";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <SessionProvider session={session}>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </SessionProvider>
   );
 }
 
