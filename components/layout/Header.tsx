f9e77cf4a1024eb7a24eb7207bfbf64f25f9309e
conuko <constantinu93@gmail.com>
2022-07-13 17:49:44 +0200

    fix: header shows sign in button when logged out

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Navbar, Container, Button } from "react-bootstrap";
 import Link from "next/link";
-import { useSession, signOut } from "next-auth/react";
+import { useSession, signOut, signIn } from "next-auth/react";
 
 const Header = () => {
   const [name, setName] = useState<string>("");
@@ -13,14 +13,36 @@ const Header = () => {
     }
   }, [session]);
 
+  if (!session) {
+    return (
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Project Management</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Link href="/login" passHref>
+              <Button onClick={() => signIn()}>Sign In</Button>
+            </Link>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+
   return (
-    <Navbar>
+    <Navbar expand="lg">
       <Container>
         <Navbar.Brand href="/">Project Management</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text className="p-3">Signed in as: {name}</Navbar.Text>
-          <Link href="/login" passHref>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          {/*           <Link href="/board" passHref>
+            <Button>Board</Button>
+          </Link> */}
+          <Navbar.Text className="px-3">Signed in as: {name}</Navbar.Text>
+          <Link href="/" passHref>
             <Button onClick={() => signOut()}>Log out</Button>
           </Link>
         </Navbar.Collapse>
